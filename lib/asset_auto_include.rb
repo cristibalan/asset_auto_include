f74235762c115024dcd0920fe847c9130c504f3e
Richard Shank <develop@zestic.com>
2009-11-19 20:31:33 +0800

    added shared assets, manual files and tests

--- lib/asset_auto_include.rb
+++ lib/asset_auto_include.rb
@@ -1,6 +1,10 @@
 module ActionView
   module Helpers
     module AssetTagHelper
+      @@aai_manual = {:javascript => [], :stylesheet => []}
+      @@aai_extension = {:javascript => 'js', :stylesheet => 'css'}
+      @@aai_delimiter  = '-'
+      
       def asset_auto_include_tags(asset_type = :javascript, options = {})
         default_options = {
           :auto_dir => "auto"
@@ -20,7 +24,7 @@ module ActionView
             options[:template_extension] ||= "sass"
             options[:template_directory] ||= File.join("stylesheets", "sass")
         end
-        to_include = []
+        @to_include = []
 
         asset_dir = File.join(RAILS_ROOT, "public", options[:directory])
         template_asset_dir = File.join(RAILS_ROOT, "public", options[:template_directory])
@@ -29,11 +33,12 @@ module ActionView
         auto_asset = File.join(asset_dir, "#{options[:auto_dir]}.#{options[:extension]}")
         template_auto_asset = File.join(template_asset_dir, "#{options[:auto_dir]}.#{options[:template_extension]}")
         if File.exist?(auto_asset) || File.exist?(template_auto_asset)
-          to_include << "#{options[:auto_dir]}.#{options[:extension]}"
+          @to_include << "#{options[:auto_dir]}.#{options[:extension]}"
         end
 
-        controller_path = controller.class.controller_path
-        action_asset = File.join(controller_path, controller.action_name)
+        controller_path = @controller.controller_path
+        action_name = @controller.action_name
+        action_asset = File.join(controller_path, action_name)
 
         # namespaces & shared assets
         parts = []
@@ -48,23 +53,70 @@ module ActionView
           template_shared_path = File.join(template_asset_dir, options[:auto_dir], "#{shared_asset}.#{options[:template_extension]}")
 
           if File.exist?(shared_path) || File.exist?(template_shared_path)
-            to_include << File.join(options[:auto_dir], "#{shared_asset}.#{options[:extension]}")
+            @to_include << File.join(options[:auto_dir], "#{shared_asset}.#{options[:extension]}")
           end
         end
 
-        # current action asset
-        template_action_path = File.join(template_asset_dir, options[:auto_dir], "#{action_asset}.#{options[:template_extension]}")
-        action_path = File.join(asset_dir, options[:auto_dir], "#{action_asset}.#{options[:extension]}")
-        if File.exist?(action_path) || File.exist?(template_action_path)
-          to_include << File.join(options[:auto_dir], "#{action_asset}.#{options[:extension]}")
+        # find action and shared action files 
+        template_action_path = File.join(template_asset_dir, options[:auto_dir], controller_path)
+        if File.directory?(template_action_path)
+          Dir.new(template_action_path).each do |file|
+            if File.extname(file) == ".#{options[:template_extension]}"
+              file.split(@@aai_delimiter).collect do |part|
+                @to_include << File.join(options[:auto_dir], controller_path, file) if File.basename(part, ".#{options[:template_extension]}") == action_name
+              end
+            end
+          end
+        end
+        
+        action_path = File.join(asset_dir, options[:auto_dir], controller_path)
+        if File.directory?(action_path) && (action_path != template_action_path)
+          Dir.new(action_path).each do |file|
+            if File.extname(file) == ".#{options[:extension]}"
+              file.split(@@aai_delimiter).collect do |part|
+                @to_include << File.join(options[:auto_dir], controller_path, file) if File.basename(part, ".#{options[:extension]}") == action_name
+              end
+            end
+          end
         end
 
+        #add the manually added files and clear them out
+        @to_include |= @@aai_manual[asset_type]
+        @@aai_manual[asset_type] = []
+        
         "<!-- auto #{asset_type.to_s} -->\n#{
-          to_include.uniq.collect { |source|
+          @to_include.uniq.collect { |source|
             self.send(options[:method], source)
           }.join("\n")
         }\n<!-- /auto #{asset_type.to_s} -->"
       end
+      
+      def register_asset_auto_include(asset_name = "", asset_type = :both)
+        register_manual(asset_name, asset_type)
+      end
+      
+      def self.register_asset_auto_include(asset_name = "", asset_type = :both)
+        register_manual(asset_name, asset_type)
+      end
+
+    private
+    def register_manual(asset_name = "", asset_type = :both)
+        if asset_name.empty?
+          return nil
+        end
+        if asset_type == :both
+          @@aai_manual[:javascript] << "#{asset_name}.#{@@aai_extension[:javascript]}"  
+          @@aai_manual[:stylesheet] << "#{asset_name}.#{@@aai_extension[:stylesheet]}"  
+        else
+          begin
+            @@aai_manual[asset_type] << "#{asset_name}.#{@@aai_extension[asset_type]}"
+          rescue
+            raise "#{asset_type} is not a valid asset type"
+          end
+        end
+        return nil
+    end
+
     end
   end
 end
--- test/asset_auto_include_test.rb
+++ test/asset_auto_include_test.rb
@@ -1,8 +1,201 @@
+RAILS_ROOT = File.dirname(__FILE__) + '/fixtures'
+require 'rubygems'
 require 'test/unit'
+require 'active_support'
+require 'active_support/test_case'
+require 'action_controller'
+require 'asset_auto_include'
 
-class JavascriptAutoIncludeTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+class AssetAutoIncludeTest < ActiveSupport::TestCase
+  include ActionView::Helpers::AssetTagHelper
+  include ActionView::Helpers::TagHelper
+
+  def test_javascript_return_with_no_matching_controller_or_action_and_no_manual
+    @controller = Class.new do
+      def controller_path
+        'not_included'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new
+    expected = "<!-- auto javascript -->\n\n<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_default_auto_include_no_action_and_no_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/auto_include.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_auto_include_namespace_action_and_no_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include/namespace'
+      end
+      def action_name
+        'index'
+      end
+    end.new
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/auto_include.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/namespace/index.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_views_auto_include_with_action_and_no_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include'
+      end
+      def action_name
+        'test'
+      end
+    end.new
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/auto_include.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/test-also.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-also-test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-test-also.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_stylesheet_return_with_auto_include_namespace_action_and_no_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include/namespace'
+      end
+      def action_name
+        'index'
+      end
+    end.new
+    expected = "<!-- auto stylesheet -->\n"
+    expected += "<link href=\"/stylesheets/auto/auto_include/namespace.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n"
+    expected += "<link href=\"/stylesheets/auto/auto_include/namespace/index.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n"
+    expected += "<!-- /auto stylesheet -->"
+    results = asset_auto_include_tags :stylesheet
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_manual_and_no_controller_or_action
+    @controller = Class.new do
+      def controller_path
+        'not_included'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new   
+    register_asset_auto_include 'manual',  :javascript
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/manual.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_stylesheet_return_with_manual_and_no_controller_or_action
+    @controller = Class.new do
+      def controller_path
+        'not_included'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new   
+    expected = "<!-- auto stylesheet -->\n"
+    expected += "<link href=\"/stylesheets/manual.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n"
+    expected += "<!-- /auto stylesheet -->"
+    register_asset_auto_include 'manual',  :stylesheet
+    results = asset_auto_include_tags :stylesheet
+    assert_equal(expected, results)
+  end
+  def test_both_return_with_manual_and_no_controller_or_action
+    @controller = Class.new do
+      def controller_path
+        'not_included'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new   
+    register_asset_auto_include 'manual'
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/manual.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    expected += "<!-- auto stylesheet -->\n"
+    expected += "<link href=\"/stylesheets/manual.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n"
+    expected += "<!-- /auto stylesheet -->"
+    results = asset_auto_include_tags :javascript
+    results += asset_auto_include_tags :stylesheet
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_views_auto_include_and_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include'
+      end
+      def action_name
+        'not_here'
+      end
+    end.new   
+    register_asset_auto_include 'manual',  :javascript
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/auto_include.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/manual.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_javascript_return_with_views_auto_include_with_action_and_manual
+    @controller = Class.new do
+      def controller_path
+        'auto_include'
+      end
+      def action_name
+        'test'
+      end
+    end.new
+    register_asset_auto_include 'manual',  :javascript
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/auto_include.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/test-also.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-also-test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-test-also.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/auto/auto_include/include-test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/manual.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
+  end
+  def test_return_with_action_only_and_manual
+    @controller = Class.new do
+      def controller_path
+        'no_controller_script'
+      end
+      def action_name
+        'test'
+      end
+    end.new   
+    register_asset_auto_include 'manual',  :javascript
+    expected = "<!-- auto javascript -->\n"
+    expected += "<script src=\"/javascripts/auto/no_controller_script/test.js\" type=\"text/javascript\"></script>\n"
+    expected += "<script src=\"/javascripts/manual.js\" type=\"text/javascript\"></script>\n"
+    expected += "<!-- /auto javascript -->"
+    results = asset_auto_include_tags :javascript
+    assert_equal(expected, results)
   end
 end
